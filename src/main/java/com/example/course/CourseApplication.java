d87c0fef9bf63a896ae9d71de77a120410cf8131
cristiano Brum <cristianolbrum@gmail.com>
2023-08-23 08:41:50 -0400

    aula 16 Banco de Dados H2 e Conectando o Angular na API Spring

---
+++ src/main/java/com/example/course/CourseApplication.java
@@ -0,0 +1,28 @@
+package com.example.course;
+
+import com.example.course.model.Course;
+import com.example.course.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository){
+		return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+			courseRepository.save(c);
+		};
+	}
+
+}
---
+++ src/main/java/com/example/course/model/Course.java
@@ -0,0 +1,41 @@
+package com.example.course.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
+    private Long id;
+    @Column(length = 200, nullable = false)
+    private String name;
+    @Column(length = 10, nullable = false)
+    private String category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
