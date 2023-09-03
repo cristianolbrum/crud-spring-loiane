e38b2029a4057ea72eea67ed44fe57ccd6784820
cristiano Brum <cristianolbrum@gmail.com>
2023-09-03 07:29:29 -0400

    aula 36 Remoção Lógica (Soft Delete)

--- src/main/java/com/example/course/model/Course.java
+++ src/main/java/com/example/course/model/Course.java
@@ -5,25 +5,37 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 @Entity
+@SQLDelete(sql = "UPDATE Course set STATUS  = 'Inativo' WHERE id = ?")
+@Where(clause = "status = Ativo")
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("_id")
     private Long id;
+
     @NotNull
     @NotBlank
     @Length(min=5, max=100)
     @Column(length = 100, nullable = false)
     private String name;
+
     @NotBlank
     @Length(min=10)
     @Pattern(regexp= "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotBlank
+    @Length(min=10)
+    @Pattern(regexp= "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 
     public Long getId() {
         return id;
